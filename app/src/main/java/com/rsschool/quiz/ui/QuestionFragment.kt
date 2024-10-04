00a3bd4d1602fbd8bc548987419ea76ded38c798
mybagfull <baezgamer01@gmail.com>
2024-10-05 01:58:21 +0300

    Gradle upgrade and basic question layout

--- app/src/main/java/com/rsschool/quiz/ui/QuestionFragment.kt
+++ app/src/main/java/com/rsschool/quiz/ui/QuestionFragment.kt
@@ -26,6 +26,6 @@ class QuestionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.question, container, false)
+        return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
 }
